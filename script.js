156ab3364119424ac4d1c5ff2b297a9ae1b89ddc
WehZB <vehovicveh@gmail.com>
2025-02-12 09:59:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function download(){
     var element = document.createElement('a');
-    element.setAttribute('href', 'V1.7.zip');
-    element.setAttribute('download', 'V1.7.zip');
+    element.setAttribute('href', 'V1.9.zip');
+    element.setAttribute('download', 'V1.9.zip');
     element.style.display = 'none';
     document.body.appendChild(element);
     element.click();
